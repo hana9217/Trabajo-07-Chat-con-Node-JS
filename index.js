ecf21cd029c1ac6f06902f4040ee19851f887781
hana9217 <60043851+hana9217@users.noreply.github.com>
2020-03-13 01:14:19 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000, function(){
+var port = process.env.PORT || 4000;
+//app.listen(port);
+var server = app.listen(port, function(){
     console.log('listening for requests on port 4000,');
 });
 
